d0d4c72bad7d8d56b22e434e8ca125a6147d3ee7
g14943ss <g14943ss@s30501.ACTSUDA>
2016-04-13 11:46:45 +0900

    こんちくわを追加しました

--- src/firsthelloworld/FirstHelloWorld.java
+++ src/firsthelloworld/FirstHelloWorld.java
@@ -18,6 +18,7 @@ public class FirstHelloWorld {
         // TODO code application logic here
         System.out.println("Hello World!");
         System.out.println("こんにちは　世界");
+        System.out.println("こんちくわ");
     }
     
 }
